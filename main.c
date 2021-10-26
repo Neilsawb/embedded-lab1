68ace3112f9a91b2532d7010ae2727df7af456f4
Neil Sawbridge <nsawbridge@gmail.com>
2021-10-26 09:50:15 +0200

    added uart init, putchar and putstr, tested putchar - Ok

--- main.c
+++ main.c
@@ -5,12 +5,21 @@
 #include <stdio.h>
 
 #define BLINK_MS 500
+#define BAUD 38400
+#define FOSC 16000000
+#define MYUBRR FOSC/16/BAUD-1
+
+char txchar = 'H';
 
 int main () {
-	DDRB |= (1<<PB3); // output pin
+	uart_init(MYUBRR); // initalise uart for transmit and receive.
+
+	//DDRB |= (1<<PB3); // output pin
 	
 	while (true) {
-		PORTB ^= (1<<PB3); // Using XOR to toggle between on and off.
+		//PORTB ^= (1<<PB3); // Using XOR to toggle between on and off.
+		//_delay_ms(BLINK_MS);
+		uart_putchar(txchar);  // transmit character using txchar.
 		_delay_ms(BLINK_MS);
 	}
 }
--- serial.c
+++ serial.c
@@ -1,4 +1,40 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "serial.h"
+#define BAUD 38400
+#define FOSC 16000000
+
+void uart_init(unsigned int ubrr) {
+	UBRR0H = (unsigned char)(ubrr>>8);
+ 	UBRR0L = (unsigned char)ubrr;
+ 	//Enable receiver and transmitter */
+ 	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+ 	/* Set frame format: 8data, 1stop bit */
+ 	UCSR0C = (1<<UCSZ01)|(1<<UCSZ00);
+}
+
+
+void uart_putchar(char chr) {
+	/* Wait for empty transmit buffer */
+ 	while ( !( UCSR0A & (1<<UDRE0)) );
+ 	/* Put data into buffer, sends the data */
+	UDR0 = chr;
+	if (chr == '\n') {
+		UDR0 = chr; // add carriage return if new line is sent.
+	}
+}
+
+void uart_putstr(const char *str)
+{
+	unsigned char j=0;
+	
+	while (str[j]!=0)		/* Send string till null */
+	{
+		uart_putchar(str[j]);	
+		j++;
+	}
+}
+
+
+
 
